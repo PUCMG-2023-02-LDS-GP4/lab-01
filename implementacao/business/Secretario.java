3ea3ac97e02bc5bfb60b334319bf85039ca157ea
Daniel Leão <daniel.rezende@sga.pucminas.br>
2023-09-02 16:11:00 -0300

    Criação da classe Secretario

---
+++ implementacao/business/Secretario.java
@@ -0,0 +1,16 @@
+package business;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Secretario extends Usuario{
+        // Construtores
+    public Secretario(String nome, String usuario, String senha, Map<Integer,Disciplina> disciplinasLecionadas) {
+        super(nome, usuario, senha);
+    }
+
+    public Secretario(String nome, String usuario, String senha) {
+        super(nome, usuario, senha);
+    }
+
+}
